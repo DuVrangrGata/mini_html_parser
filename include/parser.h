8dc652f6432f607b47cf1425d96141e24403f03d
Lucas Elsen <lucaselsen@protonmail.com>
2022-09-06 23:58:28 +0200

    Add first test for a single-header-file library
    The single-header-file library for now just contains
    a greetings Hello World function, to test the basic project structure.

---
+++ include/parser.h
@@ -0,0 +1,19 @@
+#ifndef MINI_HTML_PARSER_H
+#define MINI_HTML_PARSER_H
+
+#include <cstdio>
+
+void greeting();
+
+#ifdef MINI_HTML_PARSER_IMPLEMENTATION
+
+
+void greeting()
+{
+  std::printf("Hello World!!!\n");
+}
+
+
+
+#endif //MINI_HTML_PARSER_IMPLEMENTATION
+#endif //MINI_HTML_PARSER_H
